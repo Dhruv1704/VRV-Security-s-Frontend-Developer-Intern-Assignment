fa617d0d9f72b00fcebf25b7761f4449831b68d9
Dhruv Singh Negi <negidhruv1701@gmail.com>
2024-11-26 22:10:18 +0530

    Added Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Admin from "./Components/Admin";
+import Navbar from "./Components/Navbar";
+import AddModal from "./Components/AddModal";
+import {useState} from "react";
+import EditModal from "./Components/EditModal";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+    const [displayAddModal, setDisplayAddModal] = useState(false)
+    const [displayEditModal, setDisplayEditModal] = useState(false)
+    const [editRowId, setEditRowId] = useState()
+    const localRows = JSON.parse(localStorage.getItem("rows"));
+    const defaultRow = localRows===null?[
+        {id: "1", name: "Dhruv", creation_date: "2023-11-20", role: "Admin", permissions: ["Delete", "Read", "Write"], status: "Active"},
+        {id: "2", name: "VRV", creation_date: "2023-11-24", role: "Admin", permissions: ["Delete","Write"], status: "Active"},
+        {id: "3", name: "Rohan", creation_date: "2023-11-22", role: "User", permissions: ["Read","Write"], status: "Active"},
+        {id: "4", name: "Rahul", creation_date: "2023-11-21", role: "User", permissions: ["Write"], status: "Active"},
+        {id: "5", name: "Johnlkdsml", creation_date: "2023-11-30", role: "User", permissions: ["Write"], status: "Inactive"},
+    ]:localRows;
+    const [rows, setRows] = useState(defaultRow);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Navbar/>
+        <Admin setDisplayModal={setDisplayAddModal} rows={rows} setRows={setRows} setDisplayEditModal={setDisplayEditModal} setEditRowId={setEditRowId}/>
+          <AddModal displayAddModal={displayAddModal} setDisplayAddModal={setDisplayAddModal} rows={rows} setRows={setRows}/>
+          <EditModal displayEditModal={displayEditModal} setDisplayEditModal={setDisplayEditModal} rows={rows} editRowId={editRowId} setRows={setRows}/>
+          <ToastContainer/>
+      </>
   );
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = ()=>{
+    return(
+        <div className={"border-b-2 p-2"}>
+            <h1 className={"text-2xl ml-6 font-semibold"}>VRV SECURITIES</h1>
+        </div>
+    )
+}
+
+export default Navbar;
